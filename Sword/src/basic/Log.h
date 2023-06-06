85b4eb3088dd9b45b5cd449758fe29e87c014690
hzy <244238659@qq.com>
2023-06-06 23:24:38 +0800

    字符串

--- Sword/src/basic/Log.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-//pragma once  保证这个头文件只会被包含一次，重复包含会跳过
-void Log(const char* message);
--- Sword/src/basic/Main.cpp
+++
@@ -1,9 +0,0 @@
-//#include <iostream> //预处理语句,找到iostream文件(头文件)，并把他里面的代码复制进来,相当于引入方法
-//
-//void Log(const char* message);  //声明函数 ，编译的时候链接器会找到这个声明的方法,找不到就报错
-//
-//int main()
-//{
-//    Log("Hello World");
-//    std::cin.get();
-//}
