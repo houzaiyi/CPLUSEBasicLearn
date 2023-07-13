857072bcdbbd6ac0a26556e0438df38c09142719
hzy <244238659@qq.com>
2023-07-13 19:56:35 +0800

    代码语法学习提交

--- Sword/src/basic/ANyLearn.cpp
+++ Sword/src/basic/ANyLearn.cpp
@@ -1,12 +1,12 @@
-//#include <iostream>
-//#include <any>
-//
-//
-//int main()
-//{
-//	std::any data;//any可以存储任何类型 但是尽量不要用，类型不清楚，
-//
-//	data = std::string("hzy");
-//	std::string& string = std::any_cast<std::string&>(data);//将any类型强转为string
-//	std::cin.get();
-//}
+#include <iostream>
+#include <any>
+
+
+int main()
+{
+	std::any data;//any可以存储任何类型 但是尽量不要用，类型不清楚，
+
+	data = std::string("hzy");
+	std::string& string = std::any_cast<std::string&>(data);//将any类型强转为string
+	std::cin.get();
+}
